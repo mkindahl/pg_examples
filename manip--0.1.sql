dd12b77ab7db47aa0c7aba53976537a687f43ada
Mats Kindahl <mats@kindahl.net>
2023-05-01 17:55:36 +0200

    refactor: fixing test files
    Fixing test files and removing files added by mistake

--- manip--0.1.sql
+++ manip--0.1.sql
@@ -2,4 +2,4 @@
 \echo Use "CREATE EXTENSION parquet_fdw" to load this file. \quit
 
 CREATE PROCEDURE scan_table(relation regclass)
-LANGUAGE C AS '$libdir/scanner.so';
+LANGUAGE C AS '$libdir/manip.so';
