1427589f6a6470c0e57aca26329754b0357f2612
Mats Kindahl <mats@kindahl.net>
2025-06-10 23:19:06 +0200

    chore: make update test version independent

--- simple/sql/update.sql
+++ simple/sql/update.sql
@@ -8,9 +8,20 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA simple_catalog GRANT SELECT ON TABLES TO read
 
 SELECT nspname, defaclacl FROM pg_namespace ns JOIN pg_default_acl ON ns.oid = defaclnamespace;
 
-SELECT nspname, relname, relacl, privtype, initprivs
-FROM pg_class cl JOIN pg_namespace ns ON ns.oid = relnamespace LEFT JOIN pg_init_privs ON objoid = cl.oid
-WHERE relname = 'objects';
+WITH
+  summary AS (
+    SELECT nspname,
+           relname,
+           (aclexplode(relacl)).privilege_type AS relacl_privilege_type,
+           (aclexplode(relacl)).grantee::regrole AS relacl_grantee,
+           privtype,
+           (aclexplode(initprivs)).privilege_type AS initprivs_privilege_type,
+           (aclexplode(initprivs)).grantee::regrole AS initprivs_grantee
+      FROM pg_class cl
+      JOIN pg_namespace ns ON ns.oid = relnamespace
+ LEFT JOIN pg_init_privs ON objoid = cl.oid
+     WHERE relname = 'objects')
+SELECT * INTO saved FROM summary;
 
 SELECT nspname, nspacl, privtype, initprivs
 FROM pg_namespace ns LEFT JOIN pg_init_privs ON objoid = ns.oid
@@ -26,9 +37,28 @@ SELECT nspname, nspacl, privtype, initprivs
 FROM pg_namespace ns LEFT JOIN pg_init_privs ON objoid = ns.oid
 WHERE nspname = 'simple_catalog';
 
-SELECT nspname, relname, relacl, privtype, initprivs
-FROM pg_class cl JOIN pg_namespace ns ON ns.oid = relnamespace LEFT JOIN pg_init_privs ON objoid = cl.oid
-WHERE relname = 'objects';
+WITH
+  summary AS (
+    SELECT nspname,
+           relname,
+           (aclexplode(relacl)).privilege_type AS relacl_privilege_type,
+           (aclexplode(relacl)).grantee::regrole AS relacl_grantee,
+           privtype,
+           (aclexplode(initprivs)).privilege_type AS initprivs_privilege_type,
+           (aclexplode(initprivs)).grantee::regrole AS initprivs_grantee
+      FROM pg_class cl
+      JOIN pg_namespace ns ON ns.oid = relnamespace
+ LEFT JOIN pg_init_privs ON objoid = cl.oid
+     WHERE relname = 'objects')
+SELECT COALESCE(saved.nspname, summary.nspname) AS nspname,
+       COALESCE(saved.relname, summary.relname) AS relname,
+       COALESCE(saved.privtype, summary.privtype) AS privtype,
+       saved.relacl_privilege_type, summary.relacl_privilege_type,
+       saved.relacl_grantee, summary.relacl_grantee,
+       saved.initprivs_privilege_type, summary.initprivs_privilege_type,
+       saved.initprivs_grantee, summary.initprivs_grantee
+  FROM summary FULL JOIN saved ON row(saved.*) = row(summary.*)
+ WHERE saved.relname IS NULL OR summary.relname IS NULL;
 
 DROP EXTENSION simple;
 DROP SCHEMA simple_catalog;
