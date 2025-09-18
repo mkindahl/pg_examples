feb8e2bfa9a15c56a3c55b24602fc3774243cec8
Mats Kindahl <mats@kindahl.net>
2025-09-18 09:12:20 +0200

    chore: reporter install script using wrong name

--- reporter/reporter--0.1.sql
+++ reporter/reporter--0.1.sql
@@ -1,5 +1,5 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION worker_spi" to load this file. \quit
+\echo Use "CREATE EXTENSION reporter" to load this file. \quit
 
 CREATE FUNCTION generate_report() RETURNS jsonb AS $$
   SELECT json_build_object('tables', count(*)) FROM pg_class WHERE reltype != 0;
